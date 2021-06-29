ba5cb5214b0707c8da6cbb0ae6c244d9a0358716
QuinPoley <quin.poley@gmail.com>
2021-06-28 21:23:19 -0400

    Drawing EC2 Instance Logo Programmatically

--- AwsResources.py
+++ AwsResources.py
@@ -9,13 +9,17 @@ bl_info = {
     }
 
 import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
 #from . import ec2_maker
 #from . import lambda_maker
 #from . import dynamo_maker
 #from . import apigateway_maker
 #from . import waf_maker
 
-class DRAWAWSRESOURCE_OT_ecmaker(bpy.types.Operator):
+class DRAWAWSRESOURCE_OT_ecmaker(Operator):
     bl_idname = "mesh.ecmaker"
     bl_label = "Ec2 Instance"
     bl_description = "Draw Ec2 Instance"
@@ -32,13 +36,13 @@ class DRAWAWSRESOURCE_OT_ecmaker(bpy.types.Operator):
     # -----------------------------------------------------
     def execute(self, context):
         if bpy.context.mode == "OBJECT":
-            create_object(self, context)
+            create_ec2(self, context)
             return {'FINISHED'}
         else:
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
             return {'CANCELLED'}
 
-class DRAWAWSRESOURCE_OT_LAMBDAMAKER(bpy.types.Operator):
+class DRAWAWSRESOURCE_OT_LAMBDAMAKER(Operator):
     bl_idname = "mesh.lambda"
     bl_label = "Lambda"
     bl_description = "Draw Lambda Func"
@@ -59,7 +63,7 @@ class DRAWAWSRESOURCE_OT_LAMBDAMAKER(bpy.types.Operator):
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
             return {'CANCELLED'}
 
-class DRAWAWSRESOURCE_OT_DynamoMAKER(bpy.types.Operator):
+class DRAWAWSRESOURCE_OT_DynamoMAKER(Operator):
     bl_idname = "mesh.dynamo"
     bl_label = "DynamoDB"
     bl_description = "Draw DynamoDB"
@@ -80,7 +84,7 @@ class DRAWAWSRESOURCE_OT_DynamoMAKER(bpy.types.Operator):
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
             return {'CANCELLED'}
 
-class DRAWAWSRESOURCE_OT_APIGATEWAYMAKER(bpy.types.Operator):
+class DRAWAWSRESOURCE_OT_APIGATEWAYMAKER(Operator):
     bl_idname = "mesh.apigateway"
     bl_label = "API_Gateway"
     bl_description = "Draw API Gateway"
@@ -101,7 +105,7 @@ class DRAWAWSRESOURCE_OT_APIGATEWAYMAKER(bpy.types.Operator):
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
             return {'CANCELLED'}
 
-class DRAWAWSRESOURCE_OT_WAFMAKER(bpy.types.Operator):
+class DRAWAWSRESOURCE_OT_WAFMAKER(Operator):
     bl_idname = "mesh.webappfirewall"
     bl_label = "WAF"
     bl_description = "Draw WAF"
@@ -130,6 +134,47 @@ def create_object(self, context):
     copy = active.copy()
     scene.collection.objects.link(copy)
     copy.location = cursor
+
+def create_ec2(self, context):
+    for o in bpy.data.objects:
+        o.select_set(False)
+
+    verts = []
+    edges = []
+    faces = []
+    offset = 8 
+    for i in range(4):
+        scale_x = 0.15
+        x_buffer = 0.5
+        scale_y = 1
+        scale_z = 1 - (i/20)
+        scale_z_second = scale_z - 0.1
+            
+        #scale_x-
+        verts.append(Vector((-scale_x-(i*x_buffer), 1 * scale_y, -1 * scale_z_second)))
+        verts.append(Vector((scale_x-(i*x_buffer), 1 * scale_y, -1 * scale_z)))
+        verts.append(Vector((scale_x-(i*x_buffer), -1 * scale_y, -1 * scale_z)))
+        verts.append(Vector((-scale_x-(i*x_buffer), -1 * scale_y, -1 * scale_z_second)))
+        verts.append(Vector((-scale_x-(i*x_buffer), 1 * scale_y, 1 * scale_z_second)))
+        verts.append(Vector((scale_x-(i*x_buffer), 1 * scale_y, 1 * scale_z)))
+        verts.append(Vector((scale_x-(i*x_buffer), -1 * scale_y, 1 * scale_z)))
+        verts.append(Vector((-scale_x-(i*x_buffer), -1 * scale_y, 1 * scale_z_second)))
+
+
+        
+        faces.append([0+(offset*i), 1+(offset*i), 2+(offset*i), 3+(offset*i)])
+        faces.append([0+(offset*i), 1+(offset*i), 5+(offset*i), 4+(offset*i)])
+        faces.append([1+(offset*i), 2+(offset*i), 6+(offset*i), 5+(offset*i)])
+        faces.append([2+(offset*i), 3+(offset*i), 7+(offset*i), 6+(offset*i)])
+        faces.append([3+(offset*i), 0+(offset*i), 4+(offset*i), 7+(offset*i)])
+        faces.append([4+(offset*i), 5+(offset*i), 6+(offset*i), 7+(offset*i)])
+
+
+    mesh = bpy.data.meshes.new(name="New Object Mesh")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
 # ----------------------------------------------------------
 # Registration
 # ----------------------------------------------------------
@@ -172,3 +217,6 @@ def unregister():
         bpy.utils.unregister_class(cls)
 
     bpy.types.VIEW3D_MT_mesh_add.remove(AWSResources)
+
+if __name__ == "__main__":
+    register()
