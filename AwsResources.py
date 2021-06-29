4a17724b455c24a45ecde9582f52fb1f9ebdf44d
QuinPoley <quin.poley@gmail.com>
2021-06-29 19:39:31 -0400

    Dynamo, s3

--- AwsResources.py
+++ AwsResources.py
@@ -78,7 +78,7 @@ class DRAWAWSRESOURCE_OT_DynamoMAKER(Operator):
     # -----------------------------------------------------
     def execute(self, context):
         if bpy.context.mode == "OBJECT":
-            create_object(self, context)
+            create_dynamo(self, context)
             return {'FINISHED'}
         else:
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
@@ -120,7 +120,7 @@ class DRAWAWSRESOURCE_OT_WAFMAKER(Operator):
     # -----------------------------------------------------
     def execute(self, context):
         if bpy.context.mode == "OBJECT":
-            create_object(self, context)
+            create_S3(self, context)
             return {'FINISHED'}
         else:
             self.report({'WARNING'}, "Archimesh: Option only valid in Object mode")
@@ -193,7 +193,7 @@ def create_lambda(self, context):
     faces.append([19, 23, 20, 16])
  
 
-    mesh = bpy.data.meshes.new(name="New Object Mesh")
+    mesh = bpy.data.meshes.new(name="Lambda Mesh")
     mesh.from_pydata(verts, edges, faces)
     # useful for development when the mesh may be invalid.
     # mesh.validate(verbose=True)
@@ -235,7 +235,121 @@ def create_ec2(self, context):
         faces.append([4+(offset*i), 5+(offset*i), 6+(offset*i), 7+(offset*i)])
 
 
-    mesh = bpy.data.meshes.new(name="New Object Mesh")
+    mesh = bpy.data.meshes.new(name="Ec2 Mesh")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+    
+def create_dynamo(self, context):
+    for o in bpy.data.objects:
+        o.select_set(False)
+    verts = []
+    edges = []
+    faces = []
+    
+    offset = 20
+    z_offset = [0.35, 0.85, 1.35, 1.85]
+    zs_offset = [0, 0.45, 0.95, 1.45]
+    for i in range(4): # Number blocks that jut out
+        verts.append(Vector((0, 1, z_offset[i])))
+        verts.append(Vector((0.6, 0.8, z_offset[i])))
+        verts.append(Vector((0.9, 0.3, z_offset[i])))
+        verts.append(Vector((0.9, -0.3, z_offset[i])))
+        verts.append(Vector((0.6, -0.8, z_offset[i])))
+        verts.append(Vector((0, -1, z_offset[i])))
+        verts.append(Vector((-0.6, -0.8, z_offset[i])))
+        verts.append(Vector((-0.9, -0.3, z_offset[i])))
+        verts.append(Vector((-0.9, 0.3, z_offset[i])))
+        verts.append(Vector((-0.6, 0.8, z_offset[i])))
+        # Bottom half
+        verts.append(Vector((0, 1, zs_offset[i])))
+        verts.append(Vector((0.6, 0.8, zs_offset[i])))
+        verts.append(Vector((0.9, 0.3, zs_offset[i])))
+        verts.append(Vector((0.9, -0.3, zs_offset[i])))
+        verts.append(Vector((0.6, -0.8, zs_offset[i])))
+        verts.append(Vector((0, -1, zs_offset[i])))
+        verts.append(Vector((-0.6, -0.8, zs_offset[i])))
+        verts.append(Vector((-0.9, -0.3, zs_offset[i])))
+        verts.append(Vector((-0.9, 0.3, zs_offset[i])))
+        verts.append(Vector((-0.6, 0.8, zs_offset[i])))
+    
+
+        
+        faces.append([0+(i*offset), 1+(i*offset), 2+(i*offset), 3+(i*offset), 4+(i*offset), 5+(i*offset), 6+(i*offset), 7+(i*offset), 8+(i*offset), 9+(i*offset)])
+        faces.append([10+(i*offset), 11+(i*offset), 12+(i*offset), 13+(i*offset), 14+(i*offset), 15+(i*offset), 16+(i*offset), 17+(i*offset), 18+(i*offset), 19+(i*offset)])
+        faces.append([0+(i*offset), 10+(i*offset), 11+(i*offset), 1+(i*offset)])
+        faces.append([1+(i*offset), 11+(i*offset), 12+(i*offset), 2+(i*offset)])
+        faces.append([2+(i*offset), 12+(i*offset), 13+(i*offset), 3+(i*offset)])
+        faces.append([3+(i*offset), 13+(i*offset), 14+(i*offset), 4+(i*offset)])
+        faces.append([4+(i*offset), 14+(i*offset), 15+(i*offset), 5+(i*offset)])
+        faces.append([5+(i*offset), 15+(i*offset), 16+(i*offset), 6+(i*offset)])
+        faces.append([6+(i*offset), 16+(i*offset), 17+(i*offset), 7+(i*offset)])
+        faces.append([7+(i*offset), 17+(i*offset), 18+(i*offset), 8+(i*offset)])
+        faces.append([8+(i*offset), 18+(i*offset), 19+(i*offset), 9+(i*offset)])
+        faces.append([9+(i*offset), 19+(i*offset), 10+(i*offset), 0+(i*offset)])
+
+    mesh = bpy.data.meshes.new(name="Dynamo Mesh")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+    
+def create_S3(self, context):
+    for o in bpy.data.objects:
+        o.select_set(False)
+    verts = []
+    edges = []
+    faces = []
+    
+    offset = 8
+    squares1 = [-0.25, 1, 2.25]
+    squares2 = [-1.25, 0, 1.25]
+    for i in range(3): # Make the squares
+        verts.append(Vector((0, 1, squares1[i])))
+        verts.append(Vector((1, 1, squares1[i])))
+        verts.append(Vector((1, 0, squares1[i])))
+        verts.append(Vector((0, 0, squares1[i])))
+        verts.append(Vector((0, 1, squares2[i])))
+        verts.append(Vector((1, 1, squares2[i])))
+        verts.append(Vector((1, 0, squares2[i])))
+        verts.append(Vector((0, 0, squares2[i])))
+        
+        faces.append([0+(i*offset), 1+(i*offset), 2+(i*offset), 3+(i*offset)])
+        faces.append([4+(i*offset), 5+(i*offset), 6+(i*offset), 7+(i*offset)])
+        faces.append([0+(i*offset), 4+(i*offset), 5+(i*offset), 1+(i*offset)])
+        faces.append([1+(i*offset), 5+(i*offset), 6+(i*offset), 2+(i*offset)])
+        faces.append([2+(i*offset), 6+(i*offset), 7+(i*offset), 3+(i*offset)])
+        faces.append([3+(i*offset), 7+(i*offset), 4+(i*offset), 0+(i*offset)])
+    
+    # Background
+    verts.append(Vector((0, 2, squares1[2])))  # 24
+    verts.append(Vector((0, 2.5, squares1[2]))) #25
+    verts.append(Vector((2, 2, squares1[2])))   #26
+    verts.append(Vector((2.5, 2.5, squares1[2])))  #27
+    verts.append(Vector((2, 0, squares1[2])))     #28
+    verts.append(Vector((2.5, 0, squares1[2]))) # 29
+    verts.append(Vector((0, 2, squares2[0])))   #30
+    verts.append(Vector((0, 2.5, squares2[0])))  #31
+    verts.append(Vector((2, 2, squares2[0])))    #32
+    verts.append(Vector((2.5, 2.5, squares2[0]))) #33
+    verts.append(Vector((2, 0, squares2[0])))     #34
+    verts.append(Vector((2.5, 0, squares2[0])))   #35
+    
+    # 25 
+    faces.append([24, 26, 27, 25])
+    faces.append([24, 26, 32, 30])
+    faces.append([25, 31, 33, 27])
+    faces.append([24, 25, 31, 30])
+    faces.append([30, 31, 33, 32])
+    
+    faces.append([34, 35, 33, 32]) # bot
+    faces.append([34, 28, 26, 32]) # front
+    faces.append([35, 29, 27, 33]) # bac
+    faces.append([34, 35, 29, 28]) #side
+    faces.append([28, 29, 27, 26]) #top
+    
+    mesh = bpy.data.meshes.new(name="S3 Mesh")
     mesh.from_pydata(verts, edges, faces)
     # useful for development when the mesh may be invalid.
     # mesh.validate(verbose=True)
